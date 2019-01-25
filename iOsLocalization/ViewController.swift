c300ea8c8df864443261305a6d1f0da72e6b98d1
Gabriel Richter Reynoso <gabrielrr@Gabriels-MacBook-Pro.local>
2019-01-25 09:31:57 +0100

    First Commit

---
+++ iOsLocalization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOsLocalization
+//
+//  Created by Gabriel Richter Reynoso on 2019-01-25.
+//  Copyright © 2019 Gabriel Richter Reynoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
